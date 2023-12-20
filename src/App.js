62789edb68bfbe870c0b3595b69d10edda7c674a
Selectorv/MY PORTFOLIO <125983196+Selectorv@users.noreply.github.com>
2023-12-20 09:41:53 +0300

    commit all

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect } from "react"
+import Menu from './Components/Menu'
+import NewsGrid from './Components/NewsGrid'
+
 
 function App() {
+  const [items, setItems ] = useState([])
+  const [active, setActive] = useState (1)
+  const [category, setCategory] = useState("general")
+
+  useEffect(() => {
+  fetch (`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=3e032a9e100f432f9b207bc608d2b071`)
+    .then (res => res.json())
+    .then(data => setItems(data.articles))
+  }, [category])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1 className="title">MUSCO <span>NEWS</span></h1>
+       <Menu active={active} setActive={setActive} setCategory={setCategory}/>
+       <NewsGrid items={items}/>
+      
     </div>
+    
   );
 }
 
 export default App;
+// GET https://newsapi.org/v2/top-headlines?country=us&apiKey=3e032a9e100f432f9b207bc608d2b071
+// GET https://newsapi.org/v2/everything?q=bitcoin&apiKey=3e032a9e100f432f9b207bc608d2b071
+// GET https://newsapi.org/v2/top-headlines?country=us&apiKey=API_KEY
+
+
